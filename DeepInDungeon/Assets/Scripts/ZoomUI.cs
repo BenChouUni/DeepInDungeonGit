3a838e5fcfd4a9502099320f3fab6f91d4987782
BenChouUni <ben900202chou@gmail.com>
2022-05-02 02:22:32 +0800

    5/2ScrollUpdate
    完成deck滾動條設計

---
+++ DeepInDungeon/Assets/Scripts/ZoomUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ZoomUI : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public float zoomSize = 1.2f;
+
+    public void OnPointerEnter(PointerEventData pointerEvent)
+    {
+        transform.localScale = new Vector3(zoomSize, zoomSize, 1.0f);
+
+
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        transform.localScale = Vector3.one;
+    }
+
+}
